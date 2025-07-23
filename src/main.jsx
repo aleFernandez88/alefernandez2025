f8fa4710ef2785114a421dce5e366a245bc2c76e
Ale Fernández <alejandro.d.fernandez.88@gmail.com>
2025-07-23 11:18:11 -0300

    fix cambios en rutas

--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/alefernandez2025">
         <CssBaseline />
         <App />
       </BrowserRouter>
